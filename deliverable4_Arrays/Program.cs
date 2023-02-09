669f4ad36a754986638a0969dd36e494ef917a9f
edwar <edwar@LAPTOP-NQ107KT4>
2023-02-09 17:50:38 -0500

    Add project files.

---
+++ deliverable4_Arrays/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Name:Edward Cason
+ * Date:2/09/2023
+ * Description: Application that computes the first 25 numbers of the Fibonacci sequence 
+ */
+
+
+namespace deliverable4_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 25;
+
+            int[] fibonacci = new int[number];
+
+            fibonacci[0] = 0;
+            fibonacci[1] = 1; 
+
+            for(int i = 2; i < number; i++)
+            {
+                fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
+            }
+
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine("Fibonacci number " + i + " is: " + fibonacci[i]);
+            }
+        }
+    }
+}
